46ecfebc7364f4de20e610f7e0cb280e316f2eab
vitoria174 <vitoriaalbuquerque174@gmail.com>
2026-03-22 17:39:01 -0400

    Cricao dos eventos na main

--- eventos.py
+++ eventos.py
@@ -1,33 +1,72 @@
 import requests
 from requests import HTTPError
+from rich import print
 
+class Usuario:
 
-def user(username):
+    def __init__(self, username):
+        self.username = username
 
-    try:
-        url = requests.get(f"https://api.github.com/users/{username}/events")
-        url.raise_for_status()
-    except HTTPError as HTTPerr:
-        print(f'HTTP erro: {HTTPerr}')
-    except Exception as err:
-        print("ERRO{err}")
-    else:
-        r = url.json()
-        return r
+    def validacao(self):
+        try: 
+            url = requests.get(f"https://api.github.com/users/{self.username}/events") #faz uma requisao de usuario da api do github
+            url.raise_for_status() #verifica de o response gera um erro de 400 ao 500
+            return url.json() #retorna o json da requisicao (url)
         
+        except requests.exceptions.HTTPError as erro:
+            print(f"[red]Usuario não encontrado [/red] {erro}")
+            return None
+        
+        except Exception as erro:
+            print(f"Erro: {erro}")
+            return None
+        
+    def CommitEvent(self):
+        cont = 0
+        eventos = self.validacao()
+        if not eventos:
+             print("Usuario não encontrado")
+             return
+        
+        for evento in eventos:
+            if evento['type'] == "PushEvent":
+                cont +=1
+        print(f"Commit {cont} enviados.")
 
-def get_events(r):   
+    def CreateEvent(self):
+        ContEvent = 0
+        event_create = self.validacao()
+        
+        for create in event_create:
+            if create['type'] == 'CreateEvent':
+                ContEvent += 1
+
+        print(f"Foi criado {ContEvent} repositorio.")
     
-    lista_eventos = []
-    for evento in r:
+    def DeleteEvent(self):
+        DelEvent = 0
+        delete = self.validacao()
 
-        usuario = evento['actor']['login']
-        repositorio = evento['repo']['name']
+        for delete in delete:
+            if delete['type'] == 'DeleteEvent':
+                DelEvent += 1
+        print(f'{DelEvent} repositorio deletado.')
+        
+    def ForkEvent(self):
+        Cont_Fork = 0
+        fork_event = self.validacao()
 
-        if evento['type'] == 'PushEvent':
-            lista_eventos.append(f'Usuario {usuario} criou repositorio {repositorio}')
-    print(lista_eventos)
-       
+        for fork in fork_event:
+            if fork['type'] == 'ForkEvent':
+                Cont_Fork += 1
+        print(f'{Cont_Fork} fork no repositorio')
     
+    def WatchEvent(self):
+        Cont_Watch = 0
+        watch_event = self.validacao()
 
-get_events(user('vitoria174'))
+        for watch in watch_event:
+            if watch['type'] == 'WatchEvent':
+                Cont_Watch += 1
+        print(f"{Cont_Watch} :star: repositorio")
+
--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-import argparse as ar
-from eventos import user, get_events
+from rich.panel import Panel
+from rich import print
+from eventos import Usuario
 
-parse = ar.ArgumentParser()
 
-comando = parse.add_subparsers(dest='comando')
 
-add = comando.add_parser('get')
-add.add_argument('usuario')
+def eventos(user):
+        menu ="""
+    [blue]
+        1 - Commits
+        2 - Create Events
+        3 - Delete Events
+        4 - Fork Events
+        5 - Watch Event
+    [/blue]
+"""
 
-args = parse.parse_args()
+        print(Panel(menu, title= "Conteudo", width=100))
+        opcao= int(input("OPÇÃO: "))
 
-if args.comando == 'get':
-    n = user(args.usuario)
-    print(n)
-    if n:
-        get_events(n)
+        if opcao == 1:
+            user.CommitEvent()
+        
+        elif opcao == 2:
+             user.CreateEvent()
 
+        elif opcao == 3:
+            user.DeleteEvent()
+
+        elif opcao == 4:
+             user.ForkEvent()
+
+def main():
+    [blue]username= input("User Github: ")[/blue]
+    user = Usuario(username)
+
+    if user.validacao():
+        eventos(user)
+
+if __name__=="__main__":
+    main()
